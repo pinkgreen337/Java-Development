5e65ab97f0f16fe52ad314fe9c39646e8cf3aff2
pinkgreen337 <110970911+pinkgreen337@users.noreply.github.com>
2022-08-12 17:50:56 +0900

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,26 @@
+/* myscript. js */
+
+function hap(a, b){ //두 수 사이의 합을 반환하시오
+    if(a>b){
+        let tmp=a; a=b; b=tmp;
+    }//if end
+
+    let sum=0;
+    for(let n=a; n<=b; n++){
+        sum += n;
+    }//for end
+
+    return sum;
+}//hap() end
+
+function diff(a, b){ //두 수 사이의 차이를 반환하시오 (절대값)
+    return Math.abs(a-b);
+}//diff end
+
+function leap(year){ //윤년 평년을 반환하시오
+    if(year%4==0 && year%100!=0 || year%400==0){
+        return true;
+    }else{
+        return false;
+    }//if end
+}//leap() end
